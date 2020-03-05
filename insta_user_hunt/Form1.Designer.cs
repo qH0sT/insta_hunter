ae3695021abfa62817d4cf1a8ee696f127913797
qH0sT <asuslaptop230@gmail.com>
2020-03-05 10:04:38 +0300

    insta user hunter gui

---
+++ insta_user_hunt/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace insta_user_hunt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.kopyalaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bilgileri Bul";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(136, 127);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(154, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(412, 342);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 305);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "sagopakajmer_resmi";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kopyalaToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 26);
+            // 
+            // kopyalaToolStripMenuItem
+            // 
+            this.kopyalaToolStripMenuItem.Name = "kopyalaToolStripMenuItem";
+            this.kopyalaToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.kopyalaToolStripMenuItem.Text = "Kopyala";
+            this.kopyalaToolStripMenuItem.Click += new System.EventHandler(this.kopyalaToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 377);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "InstagramOSINT GUI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kopyalaToolStripMenuItem;
+    }
+}
+
---
+++ insta_user_hunt/Form1.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace insta_user_hunt
+{
+    public partial class Form1 : Form
+    {
+        /*
+  _________                                     ____  __.           __                      
+ /   _____/____     ____   ____ ___________    |    |/ _|____      |__| _____   ___________ 
+ \_____  \\__  \   / ___\ /  _ \\____ \__  \   |      < \__  \     |  |/     \_/ __ \_  __ \
+ /        \/ __ \_/ /_/  >  <_> )  |_> > __ \_ |    |  \ / __ \_   |  |  Y Y  \  ___/|  | \/
+/_______  (____  /\___  / \____/|   __(____  / |____|__ (____  /\__|  |__|_|  /\___  >__|   
+        \/     \//_____/        |__|       \/          \/    \/\______|     \/     \/       
+        
+             */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async Task<bool> Bul(string kullaniciAdi)
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    Process cmd = new Process();
+                    ProcessStartInfo sinfo = new ProcessStartInfo("CMD.exe",
+                    @"/C python main.py --username " + kullaniciAdi)
+                    {
+                        RedirectStandardInput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
+                    cmd.StartInfo = sinfo;
+                    cmd.Start();
+                    cmd.WaitForExit();
+                });
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+        Dictionary<string, string> translation = new Dictionary<string, string>() {
+            {"Username","Kullanıcı Adı: "},
+            {"Profile name","Profil ismi: " },
+            {"URL","Link: " },
+            {"Followers","Takipçiler: " },
+            {"Following","Takip ediyor: " },
+            {"Posts","Gönderiler: " },
+            {"Bio","Biyografi: " },
+            {"profile_pic_url","Profil foto url: " },
+            {"is_business_account","Business Hesap? " },
+            {"connected_to_fb","Facebook'a bağlı: " },
+            {"externalurl","Dış bağlantı: " },
+            {"joined_recently","Son giriş: " },
+            {"business_category_name","Business kategori ismi: " },
+            {"is_private","Özel? " },
+            {"is_verified","Doğrulandı? " }
+        };
+        string[] dataForParsing = {
+            "Username",
+            "Profile name",
+            "URL",
+            "Followers",
+            "Following",
+            "Posts",
+            "Bio",
+            "profile_pic_url",
+            "is_business_account",
+            "connected_to_fb",
+            "externalurl",
+            "joined_recently",
+            "business_category_name",
+            "is_private",
+            "is_verified"
+        };
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (await Bul(textBox1.Text))
+            {
+                try {
+                    dynamic attriubutes = JObject.Parse(File.ReadAllText(Environment.CurrentDirectory + "\\" + textBox1.Text +
+                        "\\data.txt"));
+                    for (int k = 0; k < dataForParsing.Length; k++) {
+                        listBox1.Items.Add(translation[dataForParsing[k]] + attriubutes[dataForParsing[k]].ToString().Replace("True", "Evet").
+                            Replace("False", "Hayır").Replace("None", "Hayır").Replace("Yes", "Evet"));
+                    }
+
+                    string img = (Environment.CurrentDirectory + "\\" + textBox1.Text +
+                        "\\profile_pic.jpg");
+                    pictureBox1.ImageLocation = img;
+                }
+                catch (Exception){ }
+           }
+        }
+
+        private void kopyalaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(listBox1.Items.Count > 0) {
+                Clipboard.SetText((string)listBox1.SelectedItems[0]); }
+        }
+    }
+}
